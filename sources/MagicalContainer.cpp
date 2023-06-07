bd1cd5f8bcb2fdb7b061ec7057c8f9991bd74a04
Tomer Gozlan <tomgoz96@gmail.com>
2023-06-07 03:56:46 +0300

    Implementation of all methods

---
+++ sources/MagicalContainer.cpp
@@ -0,0 +1,393 @@
+//
+// Created by Tomer Gozlan on 04/06/2023.
+//
+
+#include "MagicalContainer.hpp"
+
+
+namespace ariel {
+
+/**
+ * Adds an element to the MagicalContainer if it is not already present.
+ * @note The method will ensure that duplicate elements are not added to the vector, and the vector remains sorted after adding the new element.
+ * @param element The element to be added.
+ */
+    void MagicalContainer::addElement(int element) {
+        bool flag = false;
+        for (int i = 0; i < this->size(); ++i) {
+            if (this->elements[i] == element) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            this->elements.emplace_back(element);
+            std::sort(this->elements.begin(), this->elements.end());
+        }
+    }
+
+/**
+ * @brief Removes an element from the MagicalContainer.
+ * This function removes the specified element from the MagicalContainer if it exists.
+ * @param element The element to be removed.
+ * @throws std::runtime_error if the element is not found in the MagicalContainer.
+ */
+    void MagicalContainer::removeElement(int element) {
+        auto it = elements.begin();
+        while (it != elements.end()) {
+            if (*it == element) {
+                elements.erase(it);
+                return;
+            }
+            ++it;
+        }
+        throw std::runtime_error("Error: Element not found in MagicalContainer");
+    }
+
+/**
+ * @brief Get the number of elements in the MagicalContainer.
+ * This function returns the size of the MagicalContainer, which is the number of elements currently stored in it.
+ * @return The number of elements in the MagicalContainer.
+ */
+    int MagicalContainer::size() const {
+        return (int)this->elements.size();
+    }
+
+/**
+ * @brief Get a const reference to the elements in the MagicalContainer.
+ * @return A const reference to the std::vector<int> containing the elements in the MagicalContainer.
+ */
+    std::vector<int> MagicalContainer::getElements() const {
+        return this->elements;
+    }
+
+/**
+ * @brief Get the element at the specified index.
+ * @param index The index of the element to retrieve.
+ * @return The element at the specified index.
+ * @throws std::out_of_range if the index is out of range.
+ */
+    int MagicalContainer::getElement(int index) const {
+        if (index < 0 || index >= elements.size()) {
+            throw std::out_of_range("Error: Invalid index.");
+        }
+        return elements[static_cast<std::vector<int>::size_type>(index)];
+    }
+
+/**
+ * @brief Set the elements of the MagicalContainer.
+ * This function replaces the existing elements in the MagicalContainer with the elements provided in the newElements vector.
+ * @param newElements The vector containing the new elements to be set.
+ * @note The contents of the MagicalContainer will be completely replaced by the elements in newElements.
+ */
+    void MagicalContainer::setElements(const std::vector<int> &newElements) {
+        this->elements = newElements;
+    }
+
+
+/// Implementation of the AscendingIterator class.
+
+
+/**
+ * @brief Constructs an AscendingIterator object for the given MagicalContainer.
+ * @param container The MagicalContainer to iterate over.
+ */
+    MagicalContainer::AscendingIterator::AscendingIterator(ariel::MagicalContainer &container) : container(container),
+                                                                                                 currentIndex(0) {}
+/**
+ * @brief Copy constructor of AscendingIterator object.
+ * @param other The AscendingIterator object to copy from.
+ */
+    MagicalContainer::AscendingIterator::AscendingIterator(const ariel::MagicalContainer::AscendingIterator &other)
+
+    noexcept : container(other.container),currentIndex(other.currentIndex) {}
+
+/**
+ * @brief Destructor for the AscendingIterator object.
+ */
+    MagicalContainer::AscendingIterator::~AscendingIterator() {}
+
+/**
+* Assignment operator (=) for the AscendingIterator class.
+* @param other The AscendingIterator object to be assigned.
+* @return A reference to the current AscendingIterator object after assignment.
+*/
+AscendingIterator &
+MagicalContainer::AscendingIterator::operator=(const ariel::MagicalContainer::AscendingIterator &other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->currentIndex = other.currentIndex
+    return *this;
+}
+
+/**
+ * @brief Overloads the equality comparison operator (==) for the AscendingIterator class.
+ * @param other The AscendingIterator object to compare with.
+ * @return true if the currentIndex values are equal, false otherwise.
+ */
+bool MagicalContainer::AscendingIterator::operator==(const ariel::MagicalContainer::AscendingIterator &other) const {
+    return this->currentIndex == other.currentIndex;
+}
+
+/**
+ * @brief Overloads the inequality comparison operator (!=) for the AscendingIterator class.
+ * @param other The AscendingIterator object to compare with.
+ * @return true if the currentIndex values are not equal, false otherwise.
+ */
+bool
+MagicalContainer::AscendingIterator::operator!=(const ariel::MagicalContainer::AscendingIterator &other) const {
+    return !(*this == other);
+}
+
+/**
+ * @brief Overloads the greater than (GT) comparison operator (>) for the AscendingIterator class.
+ * @param other The AscendingIterator object to compare with.
+ * @return true if the currentIndex value of the current object is greater than the currentIndex value of the other object, false otherwise.
+ */
+
+bool MagicalContainer::AscendingIterator::operator>(const ariel::MagicalContainer::AscendingIterator &other) const {
+    return (this->currentIndex > other.currentIndex);
+}
+
+/**
+ * @brief Overloads the less than (LS) comparison operator (<) for the AscendingIterator class.
+ * @param other The AscendingIterator object to compare with.
+ * @return true if the currentIndex value of the current object is less than the currentIndex value of the other object, false otherwise.
+ */
+bool MagicalContainer::AscendingIterator::operator<(const ariel::MagicalContainer::AscendingIterator &other) const {
+    return (this->currentIndex > other.currentIndex);
+}
+
+/**
+ * @brief Overloads the dereference operator (*) for the AscendingIterator class.
+ * @return The value of the element at the current index.
+ */
+int MagicalContainer::AscendingIterator::operator*() const {
+    return container.getElement(currentIndex);
+}
+
+/**
+ * @brief Overloads the pre-increment operator (++) for the AscendingIterator class.
+ * @return Reference to the updated AscendingIterator object.
+ */
+MagicalContainer::AscendingIterator &MagicalContainer::AscendingIterator::operator++() {
+    ++currentIndex;
+    return *this;
+}
+
+/**
+ * @brief Returns an iterator pointing to the beginning of the MagicalContainer.
+ * @return An AscendingIterator object pointing to the beginning of the container.
+ */
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin() const {
+    AscendingIterator beginIter(this->container);
+    return beginIter;
+}
+
+/**
+ * @brief Returns an iterator pointing to the end of the iteration over the MagicalContainer.
+ * @return An AscendingIterator object pointing to the end of the container.
+ */
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end() const {
+    AscendingIterator it(container);
+    it.currentIndex = container.elements.size();
+    return it;
+}
+
+/**
+ * @brief Returns the current index of the iterator.
+ * @return The current index of the iterator.
+ */
+int MagicalContainer::AscendingIterator::getCurrentIndex() const {
+    return currentIndex;
+}
+
+/**
+ * @brief Sets the current index of the iterator.
+ * @param index The index to set as the current index.
+ */
+void MagicalContainer::AscendingIterator::setCurrentIndex(int index) {
+    currentIndex = index;
+}
+
+/**
+ * @brief Returns a reference to the MagicalContainer associated with the iterator.
+ * @return A reference to the MagicalContainer associated with the iterator.
+ */
+const MagicalContainer &MagicalContainer::AscendingIterator::getContainer() const {
+    return container;
+}
+
+
+/// Implementation of the SideCrossIterator class.
+
+
+
+MagicalContainer::SideCrossIterator::SideCrossIterator(const ariel::MagicalContainer &container) : container(
+        container), currentIndex(0) {}
+
+MagicalContainer::SideCrossIterator::SideCrossIterator(const ariel::MagicalContainer::SideCrossIterator &other)
+        : container(other.container), currentIndex(other.currentIndex){}
+
+MagicalContainer::SideCrossIterator::~SideCrossIterator() {}
+
+SideCrossIterator &
+MagicalContainer::SideCrossIterator::operator=(const ariel::MagicalContainer::SideCrossIterator &other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->currentIndex = other.currentIndex;
+    return *this;
+}
+
+bool MagicalContainer::SideCrossIterator::operator==(const SideCrossIterator &other) const {
+    return this->currentIndex == other.currentIndex;
+}
+
+bool MagicalContainer::SideCrossIterator::operator!=(const SideCrossIterator &other) const {
+    return !(*this == other);
+}
+
+
+bool MagicalContainer::SideCrossIterator::operator>(const SideCrossIterator &other) const {
+    return (this->currentIndex > other.currentIndex);
+
+}
+
+bool MagicalContainer::SideCrossIterator::operator<(const SideCrossIterator &other) const {
+    return (this->currentIndex < other.currentIndex);
+
+}
+
+MagicalContainer::SideCrossIterator &MagicalContainer::SideCrossIterator::operator++() {
+    if (this->currentIndex == middleIndex) {
+        setCurrentIndex(this->cont.size());
+    }
+    if (this->currentIndex < middleIndex){
+        setCurrentIndex(endIndex);
+        endIndex--;
+    }
+    if(this->currentIndex > middleIndex){
+        startIndex++;
+        setCurrentIndex(startIndex);
+    }
+    return *this;
+}
+
+int MagicalContainer::SideCrossIterator::operator*() const {
+    return container.getElement(currentIndex);
+}
+
+MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::begin() const {
+    MagicalContainer::SideCrossIterator beginIter(this->container);
+    return beginIter;
+}
+
+MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::end() const {
+    MagicalContainer::SideCrossIterator it(this->container);
+    it.currentIndex = container.elements.size();
+    return it;
+}
+
+int MagicalContainer::SideCrossIterator::getCurrentIndex() const {
+    return this->currentIndex;
+}
+
+void MagicalContainer::SideCrossIterator::setCurrentIndex(int index) {
+    this->currentIndex = index;
+}
+
+const MagicalContainer &MagicalContainer::SideCrossIterator::getContainer() const {
+    return container;
+}
+
+
+/// Implementation of the PrimeIterator class.
+
+
+
+
+MagicalContainer::PrimeIterator::PrimeIterator(const ariel::MagicalContainer &container) : container(container),
+                                                                                           currentIndex(0) {
+
+}
+
+MagicalContainer::PrimeIterator::PrimeIterator(const ariel::MagicalContainer::PrimeIterator &other) : container(
+        other.container), currentIndex(other.currentIndex) {
+}
+
+MagicalContainer::PrimeIterator::~PrimeIterator() {}
+
+bool MagicalContainer::PrimeIterator::isPrime(int num) const {
+    if (num < 2)
+        return false;
+
+    for (int i = 2; i <= sqrt(num); ++i) {
+        if (num % i == 0)
+            return false;
+    }
+    return true;
+}
+
+PrimeIterator &MagicalContainer::PrimeIterator::operator=(const ariel::MagicalContainer::PrimeIterator &other) {
+    if (this == &other) {
+        return *this;
+    }
+    this->currentIndex = other.currentIndex
+    return *this;
+}
+
+bool MagicalContainer::PrimeIterator::operator==(const PrimeIterator &other) const {
+    return this->currentIndex == other.currentIndex;
+}
+
+bool MagicalContainer::PrimeIterator::operator!=(const PrimeIterator &other) const {
+    return !(*this == other);
+}
+
+bool MagicalContainer::PrimeIterator::operator>(const PrimeIterator &other) const {
+    return (this->currentIndex > other.currentIndex);
+}
+
+bool MagicalContainer::PrimeIterator::operator<(const PrimeIterator &other) const {
+    return (this->currentIndex < other.currentIndex);
+}
+
+MagicalContainer::PrimeIterator &MagicalContainer::PrimeIterator::operator++() {
+    currentIndex++;
+    while (currentIndex < container.size() && !isPrime(container.getElement(currentIndex))) {
+        currentIndex++;
+    }
+    return *this;
+}
+
+int MagicalContainer::PrimeIterator::operator*() const {
+    return container.getElement(currentIndex);
+}
+
+MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::begin() const {
+    PrimeIterator beginIter(container);
+    beginIter.setCurrentIndex(0);
+    return beginIter;
+}
+
+MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::end() const {
+    PrimeIterator it(container);
+    it.currentIndex = container.elements.size();
+    return it;
+}
+
+int MagicalContainer::PrimeIterator::getCurrentIndex() const {
+    return this->currentIndex;
+}
+
+void MagicalContainer::PrimeIterator::setCurrentIndex(int index) {
+    currentIndex = index;
+}
+
+const MagicalContainer &MagicalContainer::PrimeIterator::getContainer() const {
+    return container;
+}
+
+}
---
+++ sources/MagicalContainer.hpp
@@ -0,0 +1,254 @@
+/**
+ * @file MagicalContainer.hpp
+ * @class MagicalContainer
+ * @brief A container that stores a collection of integers with magical properties.
+ * The MagicalContainer class provides functionality to add and remove elements
+ * retrieve the size of the container, access elements by index, and retrieve a
+ * copy of all the elements. The container is implemented using a std::vector<int>
+ * @note This file only shows declarations of the function signatures
+ * @author Tomer Gozlan
+ * @date 06/06/2023
+ */
+
+#ifndef MAGICAL_ITERATORS_MAGICALCONTAINER_HPP
+#define MAGICAL_ITERATORS_MAGICALCONTAINER_HPP
+
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+namespace ariel {
+
+    class MagicalContainer {
+    private:
+
+        std::vector<int> elements;
+
+    public:
+
+        MagicalContainer() = default;
+
+        ~MagicalContainer() = default;
+
+        MagicalContainer(const MagicalContainer &other) = default;
+
+        MagicalContainer &operator=(const MagicalContainer &other) = default;
+
+        MagicalContainer(MagicalContainer &&other)
+
+        noexcept =
+        default;
+
+        MagicalContainer &operator=(MagicalContainer &&other)
+
+        noexcept =
+        default;
+
+        void addElement(int element);
+
+        void removeElement(int element);
+
+        int size() const;
+
+        std::vector<int> getElements() const;
+
+        int getElement(int index) const;
+
+        void setElements(const std::vector<int> &newElements);
+
+/**
+ * @class AscendingIterator
+ * @brief An iterator that allows iterating over the elements of a MagicalContainer in ascending order.
+ * The AscendingIterator class provides functionality to iterate over the elements of a MagicalContainer
+ * in ascending order. It keeps track of the current index within the container and provides comparison
+ * operators to compare iterators and perform iteration operations.
+ * @author Tomer Gozlan
+ * @date 06/06/2023
+ */
+        class AscendingIterator {
+        private:
+
+            MagicalContainer &container;
+            int currentIndex;
+
+        public:
+
+            AscendingIterator(MagicalContainer &container);
+
+            AscendingIterator(const AscendingIterator &other)
+
+            noexcept;
+
+            ~AscendingIterator();
+
+            AscendingIterator &operator=(const AscendingIterator &other) ;
+
+            AscendingIterator(AscendingIterator &&other)
+
+            noexcept : container(other.container), currentIndex(other
+            .currentIndex) {}
+
+            AscendingIterator &operator=(AscendingIterator &&other)
+
+            noexcept {
+                return *this;
+            }
+
+            bool operator==(const AscendingIterator &other) const;
+
+            bool operator!=(const AscendingIterator &other) const;
+
+            bool operator>(const AscendingIterator &other) const;
+
+            bool operator<(const AscendingIterator &other) const;
+
+            AscendingIterator &operator++();
+
+            int operator*() const;
+
+            AscendingIterator begin() const;
+
+            AscendingIterator end() const;
+
+            int getCurrentIndex() const;
+
+            void setCurrentIndex(int index);
+
+            const MagicalContainer &getContainer() const;
+        };
+
+/**
+ * @class SideCrossIterator
+ * @brief An iterator that allows iterating over the elements of a MagicalContainer in a side-cross pattern.
+ * The SideCrossIterator class provides functionality to iterate over the elements of a MagicalContainer
+ * in a side-cross pattern. It keeps track of the current index within the container, the direction of movement,
+ * and provides comparison operators to compare iterators and perform iteration operations.
+ * @author Tomer Gozlan
+ * @date 06/06/2023
+ */
+
+        class SideCrossIterator {
+
+        private:
+
+            const MagicalContainer &container;
+            int currentIndex;
+            static int startIndex = 0;
+            static int endIndex = this->container.size()-1;
+            static int middleIndex = this->cont.size() / 2;
+
+        public:
+
+            SideCrossIterator(const MagicalContainer &container);
+
+            SideCrossIterator(const SideCrossIterator &other);
+
+            ~SideCrossIterator();
+
+            SideCrossIterator &operator=(const SideCrossIterator &other);
+
+            SideCrossIterator &operator=(SideCrossIterator &&other)noexcept {
+                return *this;
+            }
+
+            SideCrossIterator(SideCrossIterator &&other)
+
+            noexcept : container(other.container), currentIndex(other
+            .currentIndex) ,
+            moveFromStart(other
+            .moveFromStart) {}
+
+            bool operator==(const SideCrossIterator &other) const;
+
+            bool operator!=(const SideCrossIterator &other) const;
+
+            bool operator>(const SideCrossIterator &other) const;
+
+            bool operator<(const SideCrossIterator &other) const;
+
+            SideCrossIterator &operator++();
+
+            int operator*() const;
+
+            SideCrossIterator begin() const;
+
+            SideCrossIterator end() const;
+
+            int getCurrentIndex() const;
+
+            void setCurrentIndex(int index);
+
+            const MagicalContainer &getContainer() const;
+        };
+
+
+/**
+ * @class PrimeIterator
+ * @brief An iterator that allows iterating over the prime elements of a MagicalContainer.
+ * The PrimeIterator class provides functionality to iterate over the prime elements of a MagicalContainer.
+ * It keeps track of the current index within the container and provides comparison operators to compare iterators
+ * and perform iteration operations.
+ * @author Tomer Gozlan
+ * @date 06/06/2023
+ */
+
+        class PrimeIterator {
+        private:
+            const MagicalContainer &container;
+            int currentIndex;
+
+            bool isPrime(int num) const;
+
+        public:
+            PrimeIterator(const MagicalContainer &container);
+
+            PrimeIterator(const PrimeIterator &other);
+
+            ~PrimeIterator();
+
+            PrimeIterator &operator=(const PrimeIterator &other);
+
+            PrimeIterator &operator=(PrimeIterator &&other)
+
+            noexcept {
+                return *this;
+            }
+
+            PrimeIterator(PrimeIterator &&other)
+
+            noexcept: container(other.container), currentIndex(other
+            .currentIndex) {}
+
+            bool operator==(const PrimeIterator &other) const;
+
+            bool operator!=(const PrimeIterator &other) const;
+
+            bool operator>(const PrimeIterator &other) const;
+
+            bool operator<(const PrimeIterator &other) const;
+
+            bool operator>=(const PrimeIterator &other) const;
+
+            bool operator<=(const PrimeIterator &other) const;
+
+            PrimeIterator &operator++();
+
+            int operator*() const;
+
+            PrimeIterator begin() const;
+
+            PrimeIterator end() const;
+
+            int getCurrentIndex() const;
+
+            void setCurrentIndex(int index);
+
+            const MagicalContainer &getContainer() const;
+
+        };
+
+    };
+
+}
+
+#endif //MAGICAL_ITERATORS_MAGICALCONTAINER_HPP
